122909aaa922ec95cb2eebcc2441a77e3277a043
sravanKumar <sravankumarnc@gmail.com>
2023-06-18 00:22:22 +0530

    production

--- src/main/java/com/sravan/employee/controller/EmployeeController.java
+++ src/main/java/com/sravan/employee/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.sravan.employee.controller;
 
 import com.sravan.employee.model.Employee;
 import com.sravan.employee.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public class EmployeeController {
 
     private final EmployeeService employeeService;
 
+    @Autowired
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
