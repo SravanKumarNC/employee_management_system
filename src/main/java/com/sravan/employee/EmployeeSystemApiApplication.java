86371117d68efa6bd926ea50c22808dc1dbd37f6
sravanKumar <sravankumarnc@gmail.com>
2023-06-18 00:07:22 +0530

    final1

--- src/main/java/com/sravan/employee/EmployeeSystemApiApplication.java
+++ src/main/java/com/sravan/employee/EmployeeSystemApiApplication.java
@@ -2,8 +2,9 @@ package com.sravan.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class EmployeeSystemApiApplication {
 
 	public static void main(String[] args) {
