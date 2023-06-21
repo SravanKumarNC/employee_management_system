27943d801135f732a5717abee316e2902d9c5300
SravanKumarNC <107425365+SravanKumarNC@users.noreply.github.com>
2023-06-21 11:03:02 +0530

    Update EmployeeServiceImpl.java

--- src/main/java/com/sravan/employee/services/EmployeeServiceImpl.java
+++ src/main/java/com/sravan/employee/services/EmployeeServiceImpl.java
@@ -16,6 +16,7 @@ public class EmployeeServiceImpl implements EmployeeService{
 
     private EmployeeRepository employeeRepository;
 
+    @Autowired
     public EmployeeServiceImpl(EmployeeRepository employeeRepository){
         this.employeeRepository = employeeRepository;
     }
